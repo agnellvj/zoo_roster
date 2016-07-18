46edd592fcdff9216ef585f6d16108f29e206e3c
Vinny Agnello <agnellvj@gmail.com>
2016-07-18 09:13:20 -0600

    add another filter for converting date of birth to age.

--- client/app/components/animal/animal.js
+++ client/app/components/animal/animal.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import animalComponent from './animal.component';
+import moment from 'moment';
 
 let animalModule = angular.module('animal', [
   uiRouter
@@ -19,6 +20,13 @@ let animalModule = angular.module('animal', [
   return (gender) => {
     let genderHash = { 'm' : 'Male', 'f': 'Female' };
     return genderHash[gender];
+  }
+})
+
+.filter('agerize', () => {
+  return (dob) => {
+    let date_of_birth = moment(dob, 'MM-DD-YYYY');
+    return (moment().format("YYYY") - date_of_birth.format("YYYY"));
   }
 })
 
